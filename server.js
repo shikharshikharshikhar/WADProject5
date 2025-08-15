fddabf39703350edd90536d6fdbe2c5fe70c301a
shikharshikharshikhar <34400575+shikharshikharshikhar@users.noreply.github.com>
2025-08-14 21:22:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'));
+app.use('/auth', require('./routes/auth').router);
 app.use('/contacts', require('./routes/contacts'));
 
 // 404 handler
