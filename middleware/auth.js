58516c768e128e1a97ad905e367a1fc92d1fa37a
shikharshikharshikhar <34400575+shikharshikharshikhar@users.noreply.github.com>
2025-08-14 21:04:54 -0400

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+// Authentication middleware
+const requireAuth = (req, res, next) => {
+    if (req.session.user) {
+        next();
+    } else {
+        req.session.returnTo = req.originalUrl;
+        res.redirect('/auth/login?error=auth_required');
+    }
+};
+
+// Optional authentication - doesn't redirect, just sets user info
+const optionalAuth = (req, res, next) => {
+    res.locals.user = req.session.user || null;
+    res.locals.isAuthenticated = !!req.session.user;
+    next();
+};
+
+// Redirect if already logged in
+const redirectIfAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+};
+
+module.exports = {
+    requireAuth,
+    optionalAuth,
+    redirectIfAuthenticated
+};
